72c37d3e99d2dfcfd9f60c7caa161267925c771c
Danny Rosenblatt <dgrosenblatt@gmail.com>
2014-12-27 17:17:26 -0500

    Redirect to edit path upon initial sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to user
+    if Time.now - user.created_at < 10
+      redirect_to edit_user_path user
+    else
+      redirect_to user
+    end
   end
 
   def destroy
